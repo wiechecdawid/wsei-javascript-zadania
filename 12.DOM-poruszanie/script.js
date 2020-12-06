04393c4d98d39518fbbf25442b8556c754c2155d
wiechecdawid <wiechec.dawid@gmail.com>
2020-12-06 17:03:31 +0100

    Edit06-12-2020

--- 12.DOM-poruszanie/script.js
+++ 12.DOM-poruszanie/script.js
@@ -1,34 +1,34 @@
 //Zadanie 1
-// var buzParent = document.getElementById('buz').parentElement;
-// console.log(buzParent);
+var buzParent = document.getElementById('buz').parentElement;
+console.log(buzParent);
 
-// var bazSibling = document.getElementById('baz').previousElementSibling;
-// console.log(bazSibling);
+var bazSibling = document.getElementById('baz').previousElementSibling;
+console.log(bazSibling);
 
-// var fooChildren = document.getElementById('foo').children;
-// console.log(fooChildren);
+var fooChildren = document.getElementById('foo').children;
+console.log(fooChildren);
 
-// var fooParent = document.getElementById('foo').parentElement;
-// console.log(fooParent);
+var fooParent = document.getElementById('foo').parentElement;
+console.log(fooParent);
 
-// var fooFirstChild = document.getElementById('foo').firstElementChild;
-// console.log(fooFirstChild);
+var fooFirstChild = document.getElementById('foo').firstElementChild;
+console.log(fooFirstChild);
 
-// var fooMiddleChild = fooChildren[Math.floor(fooChildren.length / 2)];
-// console.log(fooMiddleChild);
+var fooMiddleChild = fooChildren[Math.floor(fooChildren.length / 2)];
+console.log(fooMiddleChild);
 
 //Zadanie 2
-// function printElement(id){
-//     var clickMe = document.getElementById(id);
+function printElement(id){
+    var clickMe = document.getElementById(id);
 
-//     clickMe.addEventListener('click', () => {
-//         console.log(clickMe);
-//     })
-// }
-// printElement('ex2');
+    clickMe.addEventListener('click', () => {
+        console.log(clickMe.children[0].children[0].children[0].children[1].children[0].children[0].innerText);
+    })
+}
+printElement('ex2');
 
 //Zadanie 3
-var buttons = document.querySelectorAll('button');
+var buttons = document.querySelectorAll('div#ex3 button');
 
 function programButtons(){   
     buttons.forEach(button => {
@@ -60,8 +60,8 @@ var ul = document.querySelector('#ex5 ul');
 colorDivs.forEach(div => {
     div.addEventListener('mouseover', () => {
         //Wariant 1
-        // let ulFirstChild = ul.firstElementChild;
-        // ulFirstChild.style.color = div.style.backgroundColor;
+        let ulFirstChild = ul.firstElementChild;
+        ulFirstChild.style.color = div.style.backgroundColor;
 
         //Wariant 2
         // let ulLastChild = ul.lastElementChild;
@@ -80,32 +80,16 @@ colorDivs.forEach(div => {
         // }) 
 
         //Wariant 5
-        ul.style.backgroundColor = div.style.backgroundColor;
+        //ul.style.backgroundColor = div.style.backgroundColor;
     });
 });
 
 
 //Zadanie 6
-var root = document.querySelector('#ex6');
-
-//1
-// var firstChild = root.firstElementChild;
-// var firstFirstChild = firstChild.firstElementChild;
-// var firstFirstFirstChild = firstFirstChild.firstElementChild;
-
-//2
-// var firstChild = root.firstElementChild;
-// var firstChildParent = firstChild.parentElement; //var firstChildParent = root;
-// var firstChildParentfirstChild = firstChildParent.firstElementChild; //var firstChildParentfirstChild = firstChild;
-// var firstFirstChild = firstChild.firstElementChild;
-// var firstFirstBrother = firstFirstChild.nextElementSibling;
-// var firstFirstBrotherParent = firstFirstBrother.parentElement;
-
-//3
-var rootParent = root.parentElement;
-var rootParentFirstChild = rootParent.firstElementChild;
-var rootParentFirstChildParent = rootParent;
-var rootParentSecondChild = rootParent.children[1];
-var secondFirstChild = rootParentSecondChild.firstElementChild;
-var secondFirstFirstChild = secondFirstChild.firstElementChild;
-var secondFirstFirstFirstChild = secondFirstFirstChild.firstElementChild;
+const ex6 = document.querySelector("#ex6");
+const thirdChild = ex6.children[0].children[0].children[0];
+console.log(thirdChild);
+const comb2 = ex6.children[0].parentNode.children[0].nextSibling.parentNode;
+console.log(comb2);
+const comb3 = ex6.parentNode.children[0].parentNode.children[1].children[0].children[0].children[0];
+console.log(comb3);
